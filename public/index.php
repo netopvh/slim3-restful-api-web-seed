c32f69bf86d26a5dbfe08baaea018fb0dc61ba8e
Dyer, Andy <andrew.dyer@sky.uk>
2018-09-05 10:36:00 +0100

    Added middleware

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/../bootstrap/app.php';
 require_once __DIR__ . '/../bootstrap/container.php';
-require_once __DIR__ . '/../routes/api.php';
-require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../bootstrap/middleware.php';
+
+foreach (glob(__DIR__.'/../routes/*.php') as $route) {
+    require_once $route;
+}
 
 $app->run();
