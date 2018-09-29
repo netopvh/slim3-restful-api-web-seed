4b9a2bd1346752e1cd75c4992d61c8c5d4b80dee
andrewdyer <andrewdyer@10.0.0.8>
2018-09-30 00:20:26 +0100

    Updated doc blocks

--- app/Interfaces/MiddlewareInterface.php
+++ app/Interfaces/MiddlewareInterface.php
@@ -17,6 +17,10 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 interface MiddlewareInterface
 {
     /**
+     * Manipulates the Request and Response objects. You MUST return an instance of
+     * \Psr\Http\Message\ResponseInterface and should invoke the next middleware,
+     * passing it Request and Response objects as arguments.
+     * 
      * @param Request  $request
      * @param Response $response
      * @param callable $next
